a4ab39da4c62b2f2e3a9c6061135bdfae7227ba0
RIDHESH Mahajan <149393000+Ridhesh927@users.noreply.github.com>
2026-08-13 20:45:03 +0530

    Create 66-PlusOne

---
+++ 66-PlusOne/66-PlusOne.js
@@ -0,0 +1,16 @@
+// Last updated: 8/13/2026, 8:45:02 PM
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    for (let i = digits.length - 1; i>= 0;i--) {
+
+        if(digits[i]<9) {
+            digits[i]++
+            return digits
+        }
+        digits[i] = 0;
+    }
+    return [1, ...digits];
+};
