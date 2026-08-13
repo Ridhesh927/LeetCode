3c9b14be0435cd4b8ca877103e0c1299438153fe
RIDHESH Mahajan <149393000+Ridhesh927@users.noreply.github.com>
2026-08-13 20:45:01 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.js
@@ -0,0 +1,20 @@
+// Last updated: 8/13/2026, 8:44:59 PM
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    
+    if(n === 1 ) return 1;
+    if(n === 2) return 2;
+
+    let prev1 = 2;
+    let prev2 = 1;
+
+    for(let i=3; i <= n;i++) {
+        let current = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = current;
+    }
+    return prev1;
+};
